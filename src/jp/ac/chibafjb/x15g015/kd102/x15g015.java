02a0f999bc7939b792b3099c6e5f81da4181aa80
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-11-02 14:40:36 +0900

    もっと真面目に勉強します。

---
+++ src/jp/ac/chibafjb/x15g015/kd102/x15g015.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g015.kd102;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("1船橋太郎");
+		System.out.println("2船橋太郎");
+		System.out.println("3船橋太郎");
+		System.out.println("4船橋太郎");
+		System.out.println("5船橋太郎");
+		System.out.println("6船橋太郎");
+		System.out.println("7船橋太郎");
+		System.out.println("8船橋太郎");
+		System.out.println("9船橋太郎");
+		System.out.println("10船橋太郎");
+
+
+	}
+
+}
