7f126fd00cff457b3bf4ab3296b042d0e98784c9
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-11-09 14:55:18 +0900

    熊本かえってなにしよう・・・

---
+++ src/jp/ac/chibafjb/x15g015/kdT02/x15g015.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g015.kdT02;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=100;i<103;i=i+1)
+			System.out.println(i);
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g015/kdT03/x15g015.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g015.kdT03;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=55;i<=70;i=i+5)
+			System.out.println(i);
+	}
+
+}
