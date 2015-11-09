9acf53225e1085a08c030be1dbeaa757b6c226a6
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-11-09 14:04:59 +0900

    「；」付け忘れてました（笑）

---
+++ src/jp/ac/chibafjb/x15g015/kd111/x15g015.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g015.kd111;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<10;i=i+1)
+			System.out.println("船橋太郎");
+
+	}
+
+}
