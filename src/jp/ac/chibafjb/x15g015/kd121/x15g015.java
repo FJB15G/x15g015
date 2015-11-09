60e5c5379e3021ba5b07e7f4ed576c8cf282683e
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-11-09 14:44:03 +0900

    結果にコミットする「ライザップ」

---
+++ src/jp/ac/chibafjb/x15g015/kd121/x15g015.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g015.kd121;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=1;i<=10;i=i+1)
+			System.out.println(i);
+	}
+
+}
