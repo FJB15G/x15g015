3c8a63930c81a183ae9c56f7e0209805b14a469d
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-11-09 14:30:07 +0900

    結構難しかった。。。

---
+++ src/jp/ac/chibafjb/x15g015/kd112/x15g015.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g015.kd112;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<100;i=i+1)
+			System.out.println("船橋情報ビジネス専門学校");
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g015/kd115/x15g015.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g015.kd115;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<2;i=i+1)
+			System.out.println("あいうえお　あいうえお　あいうえお");
+		for(i=0;i<2;i=i+1)
+			System.out.println("かきくけこ　かきくけこ　かきくけこ");
+		for(i=0;i<2;i=i+1)
+			System.out.println("さしすせそ　さしすせそ　さしすせそ");
+	}
+
+}
