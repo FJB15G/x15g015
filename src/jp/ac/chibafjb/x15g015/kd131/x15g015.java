6d1e8ef1c00f53ddefc68bd55af34080a859cdef
unknown <x15g015@PC505N021.chiba-fjb.ac.jp>
2015-11-12 11:21:11 +0900

    眠いです

---
+++ src/jp/ac/chibafjb/x15g015/kd131/x15g015.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g015.kd131;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		x = 10;
+		System.out.println(x);
+	}
+
+}
