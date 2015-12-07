a956ea5921dbd8e5c9db6899362f9210ca765944
unknown <x15g015@PC504N001.chiba-fjb.ac.jp>
2015-12-07 14:47:19 +0900

    ブリーチ

---
+++ src/jp/ac/chibafjb/x15g015/kd151/x15g015.java
@@ -0,0 +1,24 @@
+package jp.ac.chibafjb.x15g015.kd151;
+
+import java.util.Scanner;
+
+public class x15g015 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sin = new Scanner(System.in);
+			
+			System.out.print("aの入力　-->");
+			float a = sin.nextFloat();
+			System.out.print("bの入力　-->");
+			float b = sin.nextFloat();
+			
+		
+		
+		
+		System.out.println(a+" + "+b+" = "+(a+b));
+		System.out.println(a+" - "+b+" = "+(a-b));
+		System.out.println(a+" * "+b+" = "+(a*b));
+		System.out.println(a+" / "+b+" = "+(a/b));
+	}
+}
